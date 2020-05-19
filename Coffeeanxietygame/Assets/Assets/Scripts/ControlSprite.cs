5c807a6a5e37f2c528bed8eb3e84b186e8300f66
Enmaz <37573893+Enmaz@users.noreply.github.com>
2020-05-19 17:47:21 +1000

    Movement
    Added a script for player movement, and put in colliders to keep the player onscreen

---
+++ Coffeeanxietygame/Assets/Assets/Scripts/ControlSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSprite : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigid;
+    void Start(){
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigid.velocity = new Vector2(moveHorizontal*speed, moveVertical*speed);
+    }
+}
