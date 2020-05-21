91601bf5179c02498772a7130509a67f85c026fa
Enmaz <37573893+Enmaz@users.noreply.github.com>
2020-05-21 14:43:12 +1000

    Bullets randomised

--- Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
+++ Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
@@ -18,16 +18,17 @@ public class Bulletfire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Fire", 0f, 2f);    
+        InvokeRepeating("Fire", 0f, 0.1f);    
     }
 
     private void Fire()
     {
         float angleStep = (endangle - startangle) / bulletsamount;
         float angle = startangle;
+        var zAngle = Random.Range(60, 120);
 
-        for (int i = 0; i < bulletsamount + 1; i++)
-        {
+        //for (int i = 0; i < bulletsamount + 1; i++)
+        //{
             float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
             float bulDirY = transform.position.x + Mathf.Cos((angle * Mathf.PI) / 180f);
 
@@ -36,12 +37,12 @@ public class Bulletfire : MonoBehaviour
 
             GameObject bul = Bulletpoolscript.bulletpoolinstance.GetBullet(); //Retrieve a bullet, make it active and fly in the right direction. 
             bul.transform.position = transform.position;
-            bul.transform.Rotate(0, 0, 90, Space.Self);
+            bul.transform.Rotate(0, 0, zAngle, Space.Self);
             bul.SetActive(true);
             bul.GetComponent<Bulletscript>().setMoveDirection(bulDir);
 
             angle += angleStep;
-        }
+      //  }
     }
     // Update is called once per frame
     void Update()
