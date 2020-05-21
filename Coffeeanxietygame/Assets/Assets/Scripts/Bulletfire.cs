012b4a5a89685392c9bcdbdc1b1643f544045b42
Cross <30955151+chaoszeroomega@users.noreply.github.com>
2020-05-21 12:47:58 +1000

    Bullet pool physics added
    Currently firing bullets in the wrong direction. Need to fix.

---
+++ Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletfire : MonoBehaviour
+{
+    [SerializeField]
+    private int bulletsamount = 10;
+
+    [SerializeField]
+    private float startangle = 359f, endangle = 180f;
+
+    private Vector2 bulletmovedirection;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 0f, 2f);    
+    }
+
+    private void Fire()
+    {
+        float angleStep = (endangle - startangle) / bulletsamount;
+        float angle = startangle;
+
+        for (int i = 0; i < bulletsamount + 1; i++)
+        {
+            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
+            float bulDirY = transform.position.x + Mathf.Cos((angle * Mathf.PI) / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = Bulletpoolscript.bulletpoolinstance.GetBullet(); //Retrieve a bullet, make it active and fly in the right direction. 
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bulletscript>().setMoveDirection(bulDir);
+
+            angle += angleStep;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Coffeeanxietygame/Assets/Assets/Scripts/Bulletscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletscript : MonoBehaviour
+{
+    private Vector2 movedirection;
+    private float moveSpeed;
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movedirection * moveSpeed * Time.deltaTime);   
+    }
+
+    public void setMoveDirection(Vector2 dir)
+    {
+        movedirection = dir;
+    }
+    private void OnDestroy()
+    {
+        gameObject.SetActive(false);
+    }
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
