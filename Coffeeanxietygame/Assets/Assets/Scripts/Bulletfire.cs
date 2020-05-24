af156d76943175b9fd1a5a688170dc67dcfb7693
Enmaz <37573893+Enmaz@users.noreply.github.com>
2020-05-24 16:20:53 +1000

    1-5 to switch bullet patters

--- Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
+++ Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
@@ -17,6 +17,8 @@ public class Bulletfire : MonoBehaviour
     private float startangle = 359f, endangle = 180f;
 
     private Vector2 bulletmovedirection;
+    private bool xMove = false;
+    private bool yMove = false;
 
 
 
@@ -29,7 +31,7 @@ public class Bulletfire : MonoBehaviour
         level = "one";
         //InvokeRepeating("FireMove", 0f, 0.4f);
         //InvokeRepeating("FireSpread", 0f, 0.2f);
-        InvokeRepeating("FireSpread", 0f, 0.4f);
+        //InvokeRepeating("FireSpread", 0f, 0.4f);
         //InvokeRepeating("FireCurve", 0f, 2f);
         //InvokeRepeating("FireRandom", 0f, 0.4f);
     }
@@ -181,8 +183,50 @@ public class Bulletfire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //InvokeRepeating("FireMove", 0f, 0.4f);
+        //InvokeRepeating("FireSpread", 0f, 0.2f);
+        //InvokeRepeating("FireSpread", 0f, 0.4f);
+        //InvokeRepeating("FireCurve", 0f, 2f);
+        //InvokeRepeating("FireRandom", 0f, 0.4f);
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            xMove = false;
+            yMove = false;
+            CancelInvoke();
+            InvokeRepeating("FireMove", 0f, 2f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)){
+            xMove = false;
+            yMove = false;
+            CancelInvoke();
+            InvokeRepeating("FireCurve", 0f, 2f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)){
+            zAngle = 60;
+            xMove = false;
+            yMove = false;
+            CancelInvoke();
+            InvokeRepeating("FireSpread", 0f, 0.4f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)){
+            xMove = false;
+            yMove = true;
+            CancelInvoke();
+            InvokeRepeating("FireMove", 0f, 0.4f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5)){
+            xMove = true;
+            yMove = false;
+            CancelInvoke();
+            InvokeRepeating("FireRandom", 0f, 0.4f);
+        }
+        if (yMove){
+            transform.position = new Vector3(transform.position.x, yCurve.Evaluate((Time.time)), transform.position.z);
+        }
+        if (xMove){
+            transform.position = new Vector3(xCurve.Evaluate((Time.time)), transform.position.y, transform.position.z);
+        }
         //transform.position = new Vector3(transform.position.x, yCurve.Evaluate((Time.time)), transform.position.z);
-        transform.position = new Vector3(xCurve.Evaluate((Time.time)), transform.position.y, transform.position.z);
+        //transform.position = new Vector3(xCurve.Evaluate((Time.time)), transform.position.y, transform.position.z);
         //transform.RotateAround(new Vector3(-1, -2, 0), new Vector3(0, 0, 1), 20 * Time.deltaTime);
     }
 }
