715cc4e9d42242d13181af9ef60375486cd989b4
Enmaz <37573893+Enmaz@users.noreply.github.com>
2020-06-04 13:42:02 +1000

    Cutscene transitions fixed

--- Coffeeanxietygame/Assets/Assets/Scripts/ButtonScript.cs
+++ Coffeeanxietygame/Assets/Assets/Scripts/ButtonScript.cs
@@ -28,12 +28,30 @@ public class ButtonScript : MonoBehaviour
         LevelManager.Level = "three";
         StartCoroutine(LoadScene());
     }
+
+    public void main(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void restart(){
+        StartCoroutine(ReloadLoadScene());
+    }
     IEnumerator LoadScene()
     {
         //Coroutine that loads the scene using the set scene name.    
         sceneSition.SetTrigger("end");
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(1);
+        sceneSition.ResetTrigger("end");
+
+    }
+
+    IEnumerator ReloadLoadScene()
+    {
+        //Coroutine that loads the scene using the set scene name.    
+        sceneSition.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(2);
         sceneSition.ResetTrigger("end");
 
     }
