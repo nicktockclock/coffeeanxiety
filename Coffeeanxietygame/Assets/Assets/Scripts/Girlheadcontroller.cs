f836ee151ccd86da28aaa63591206fc4c3506877
Cross <30955151+chaoszeroomega@users.noreply.github.com>
2020-06-08 20:36:44 +1000

    Added Girlhead scripts
    Bullet functionality is most likely broken, need to fix

--- Coffeeanxietygame/Assets/Assets/Scripts/Girlheadcontroller.cs
+++ Coffeeanxietygame/Assets/Assets/Scripts/Girlheadcontroller.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Girlheadcontroller : MonoBehaviour
 {
+    [SerializeField] GameObject loveBullet = null;
+    public Animator girlhead;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DelayedDestroy();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator DelayedDestroy()
+    {
+        GameObject.Instantiate(loveBullet);
+        girlhead.SetTrigger("hasSpawned");
+        yield return new WaitForSeconds(4.0f);
+        Destroy(gameObject);
+
+
     }
 }
