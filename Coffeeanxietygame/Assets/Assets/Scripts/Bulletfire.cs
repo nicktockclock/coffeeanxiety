e9dd219f3083bbc05a7d14df37632553e6053ee0
Enmaz <37573893+Enmaz@users.noreply.github.com>
2020-07-01 18:31:30 +1000

    Clean up UI

--- Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
+++ Coffeeanxietygame/Assets/Assets/Scripts/Bulletfire.cs
@@ -209,7 +209,7 @@ public class Bulletfire : MonoBehaviour
 
     IEnumerator LevelThreePattern(){
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1.5f);
         words.generateConfidence();
         xMove = true;
         while (true){
